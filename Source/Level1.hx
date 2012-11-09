a369db3fbe759bab073ea2cf3159295f20b8bed2
Indrajith K L <mac91112@gmail.com>
2012-11-09 19:25:03 +0530

    updated level and added player

--- Source/Level1.hx
+++ Source/Level1.hx
@@ -9,16 +9,36 @@ import org.flixel.FlxState;
 import org.flixel.FlxG;
 import org.flixel.FlxTilemap;
 import org.flixel.tmx.TmxMap;
+import org.flixel.FlxSprite;
 class Level1 extends FlxState
 {
 	var tilemap:FlxTilemap;
+	var player:FlxSprite;
 	override public function create():Void
 	{
+		player = new FlxSprite();
+		
 		var tmx:TmxMap = new TmxMap(Assets.getText("maps/mylevel.tmx"));
 		tilemap = new FlxTilemap();
 		var mapCsv:String = tmx.getLayer('layer1').toCsv(tmx.getTileSet('tileset'));
 		tilemap.loadMap(mapCsv, "assets/maps/tiles.png", 8, 8, FlxTilemap.OFF);
 		add(tilemap);
+		player.loadGraphic("assets/player.png");
+		player.y = 25;
+		player.x = 25;
+		add(player);
+		player.acceleration.y = 200;
+	}
+	
+	override public function destroy():Void
+	{
+		
+	}
+	
+	override public function update():Void
+	{
+		super.update();
+		FlxG.collide(player, tilemap);
 	}
 
 	
